6da5e0095ebfac675e38b918a890a3d94cdc673a
ArielBalcas <107375584+ArielBalcas@users.noreply.github.com>
2022-10-26 20:28:06 -0300

    first commit

---
+++ src/componentes/ofertas.js
@@ -0,0 +1,85 @@
+import React from 'react';
+
+
+
+        <div className='contenido-navbar'>
+
+            <div className='barradenavegacion'>
+               <img className='icono' src='https://fascinating-meringue-f9e742.netlify.app/pastel/premier-league-logo-header-full.png' alt='premier.png'></img>
+               <input className='input' type="text" name="buscador" id="buscador" placeholder="Buscar..."/>
+             <div className='navbar'>
+                <a href=" ">INICIO</a>
+                
+                <a href="./componentes/ofertas ">OFERTAS</a>
+                <a href=" ">MAPS</a>
+                <a href=" ">CONTACTOS</a>
+                <a href=" ">E.S.C</a>
+                </div>   
+               
+               
+
+                
+                 </div>
+                 <div className='fotodebarra'> 
+             <img className='prefoto' src='https://tpc.googlesyndication.com/simgad/5436685824493155914' alt='premier'></img>
+             
+            </div>
+           <div className='subtitulo'>
+             <h2> CAMISETAS</h2></div> 
+
+
+               <div className='contenedorcard'> 
+              
+<div className='card'>
+  <img className='foto' src="https://tiendagolperu.com/wp-content/uploads/2020/02/AJAX-NEGRO.jpg" alt="ajax" />
+  <div className='container'>
+    <h4><b>Camiseta del Ajax 2020/2021 </b></h4> 
+    <p>Segunda Equipacion</p>
+    <button>$121</button> 
+    
+  </div>
+</div>
+
+
+<div className='card'>
+  <img className='foto' src="https://http2.mlstatic.com/D_NQ_NP_723849-MLA50832930584_072022-O.webp" alt="city" />
+  <div className='container'>
+    <h4><b>Camiseta del Manchester city 2022/2023</b></h4> 
+    <p>Primera Equipacion
+        </p> 
+       
+        <button>$221</button>
+  </div>
+</div>
+<div className='card'>
+  <img className='foto' src="https://http2.mlstatic.com/D_NQ_NP_2X_899145-MLA51220677426_082022-F.webp" alt="psg" />
+  <div className='container'>
+    <h4><b>Camiseta  del  messi Saint-Germain 2022/2023</b></h4> 
+    <p>Primera Equipacion</p> 
+    
+    <button>$199</button>
+  </div>
+</div>
+
+<div className='card'>
+  <img className='foto' src="https://s1.eestatic.com/2021/06/01/elbernabeu/real-madrid/futbol/585702192_186795589_1280x1706.jpg" alt="realmadrid" />
+  <div className='container'>
+    <h4><b>Camiseta del Real Madrid 2022/2023</b></h4> 
+    <p>Primera Equipacion</p> 
+    <button>$189</button>
+  </div>
+</div>
+                </div>
+         
+
+         
+
+
+        </div>
+
+  
+export default pruevauno;
+
+
+
+    
